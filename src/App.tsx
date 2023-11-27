86ed919d30d224e3994c5da228a7458d187c855f
Arkadiy <webdev-projects@yandex.ru>
2023-11-27 10:42:55 +0300

    added HH logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../../vite.svg';
+import viteLogo from './assets/vite.svg';
+import hhLogo from './assets/hh.ru.svg';
 import './App.css';
 
 function App() {
@@ -12,11 +13,14 @@ function App() {
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        <a href="hh.ru" target="_blank" rel="noreferrer">
+          <img src={hhLogo} className="logo" alt="HH logo" />
+        </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>HH HomeWork - Infrastructure</h1>
       <div className="card">
         <button
           type="button"
